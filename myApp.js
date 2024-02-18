c4e8a204825c57719c749f53202649dfd7e28faa
Fran Panteli <francescapanteli@gmail.com>
2024-02-18 13:31:17 +0000

    This commit contains my work for question 2.

--- myApp.js
+++ myApp.js
@@ -4,37 +4,10 @@ let app = express();
 // Question 1, print "Hello World" in the console
 console.log("Hello World");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Question 2, 
+app.get('/', (req, res) => {
+    res.send("Hello Express");
+});
 
 
  module.exports = app;
